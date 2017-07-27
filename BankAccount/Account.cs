b7dbffa736bf979fc14c360ccbcfbe9eed74e9a2
marwoch <marwoch@users.noreply.github.com>
2017-07-27 14:52:39 -0700

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// This represents a bank account
+    /// </summary>
+    public class Account
+    {
+        private double balance;
+
+        public Account()
+        {
+
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        /// <summary>
+        /// Adds an amount to the balance
+        /// </summary>
+        /// <param name="amt">the amount to be added</param>
+        public void Deposit(double amt)
+        {
+            if (amt > 0)
+            {
+                balance += amt;
+            }
+            else
+            {
+                throw new ArgumentException("Deposit amount must be a positive number");
+            }
+
+        }
+
+        /// <summary>
+        /// subtracts an amount from the balance
+        /// </summary>
+        /// <param name="amt">the amount to be withdrawn</param>
+        public void Withdrawl(double amt)
+        {
+            balance += amt;
+        }
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void DepositPositiveAmount()
+        {
+            //arrange
+            Account acc = new Account();
+            double initialBalance = 0;
+            double depositAmount = 10;
+            double expectedBalance = initialBalance + depositAmount;
+
+            //act
+            acc.Deposit(depositAmount);
+
+            //assert
+            Assert.AreEqual(expectedBalance, acc.Balance);
+
+            ////make it fail until ready to code...
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DepositNegativeAmountShouldThrowException()
+        {
+            //arrange
+            Account a = new Account();
+            double depositAmount = -1;
+
+            //act
+            a.Deposit(depositAmount);
+
+            //assert
+                //assert is handled by expectedException attribute
+        }
+    }
+}
